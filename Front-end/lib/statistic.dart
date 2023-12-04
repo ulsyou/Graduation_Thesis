3631863391d60919008753e5e8ddbe08ace924b0
Bui Hong Nhat <ulsyou1@gmail.com>
2023-12-04 13:25:27 +0700

    4/12

---
+++ Front-end/lib/statistic.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class StatisticsDashboard extends StatefulWidget {
+  @override
+  _StatisticsDashboardState createState() => _StatisticsDashboardState();
+}
+
+class _StatisticsDashboardState extends State<StatisticsDashboard> {
+  Map<String, dynamic>? statistics; // Make statistics nullable
+
+  @override
+  void initState() {
+    super.initState();
+    fetchStatistics();
+  }
+
+  fetchStatistics() async {
+    final response = await http.get(Uri.parse('http://10.0.2.2:5000/statistics')); // Use Uri.parse
+
+    if (response.statusCode == 200) {
+      setState(() {
+        statistics = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load statistics');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Statistics Dashboard'),
+      ),
+      body: statistics == null
+          ? CircularProgressIndicator()
+          : ListView.builder(
+              itemCount: statistics!.length, // Use null-aware access
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(statistics!.keys.elementAt(index)), // Use null-aware access
+                  subtitle: Text(statistics!.values.elementAt(index).toString()), // Use null-aware access
+                );
+              },
+            ),
+    );
+  }
+}
