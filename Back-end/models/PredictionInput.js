62cfe7bbc182031df4f4286d154648487665016b
Bui Hong Nhat <ulsyou1@gmail.com>
2023-12-10 11:18:34 +0700

    10/12

---
+++ Back-end/models/PredictionInput.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const PredictionInputSchema = new mongoose.Schema({
+  precipitation: Number,
+  temperature: Number,
+  humidity: Number,
+  windSpeed: Number,
+  solarRadiation: Number,
+  area: Number,
+  N: Number,
+  P: Number,
+  K: Number
+});
+
+module.exports = mongoose.model('PredictionInput', PredictionInputSchema);
